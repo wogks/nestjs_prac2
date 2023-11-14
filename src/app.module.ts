636249565851aa5fa6f4e74f76fdd03a807a98cf
wogks <wogks27@naver.com>
2023-11-15 00:14:17 +0900

    Feat: static file serving

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,6 +10,8 @@ import { AuthModule } from './auth/auth.module';
 import { CommonModule } from './common/common.module';
 import { APP_INTERCEPTOR } from '@nestjs/core';
 import { ConfigModule } from '@nestjs/config';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { PUBLIC_FOLDER_PATH } from './common/const/path.const';
 
 @Module({
   imports: [
@@ -18,6 +20,10 @@ import { ConfigModule } from '@nestjs/config';
       isGlobal: true,
     }),
     PostsModule,
+    ServeStaticModule.forRoot({
+      rootPath: PUBLIC_FOLDER_PATH,
+      serveRoot: '/public',
+    }),
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: '127.0.0.1',
